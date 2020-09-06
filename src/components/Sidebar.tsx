41cc66eca58324f222c17106a24d56ec4e9d6a60
Thomas Schallert <thomas.schallert@fhnw.ch>
2020-09-06 19:17:33 +0200

    add sidebar component - fetch only channels that user is subscribed to

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import gql from 'graphql-tag';
+import { useQuery } from 'react-apollo';
+
+const USER_CHANNELS = gql`
+  query {
+    user_channels(order_by: { channel: { id: asc } }) {
+      channel_id
+      user_id
+      channel {
+        name
+      }
+    }
+  }
+`;
 
 const Sidebar: React.FC<any> = () => {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, user, isLoading } = useAuth0();
+
+  let { channel } = useParams();
+
+  const { data, error, loading, client } = useQuery(USER_CHANNELS);
 
   return (
-    <ul>
-      <li>
-        <Link to="/channel/general">General</Link>
-      </li>
-      <li>
-        <Link to="/channel/casfee20">Casfee20</Link>
-      </li>
-      <li>
-        <Link to="/channel/projekt">Projekt</Link>
-      </li>
-      <li>
-        <Link to="/channel/bärbels-world">Bärbels-World</Link>
-      </li>
-    </ul>
+    <React.Fragment>
+      <ul>
+        {!loading && data && data.user_channels
+          ? data.user_channels.map((data: any) => {
+              return (
+                <li>
+                  <Link to={'/channel/' + data.channel.name}>
+                    {data.channel.name}
+                  </Link>
+                </li>
+              );
+            })
+          : ''}
+      </ul>
+    </React.Fragment>
   );
 };
 
