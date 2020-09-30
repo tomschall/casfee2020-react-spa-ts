10029390fe008b5f910a46e47ba48ce9d99d52b1
webrooster <roland.vonaesch@fhnw.ch>
2020-09-30 13:38:09 +0200

    AddChannel -> clear field on successfully adding new channel

--- src/components/addChannel/index.tsx
+++ src/components/addChannel/index.tsx
@@ -79,6 +79,7 @@ const AddChannel: React.FC = () => {
         },
       });
       setOpen(false);
+      setChannelName('');
     } catch (e) {
       setChannelName('');
       console.log('error on mutation');
