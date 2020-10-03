3736ac625db1231cc1ac5592824d1552fc360519
webrooster <roland.vonaesch@fhnw.ch>
2020-10-03 17:13:03 +0200

    Adding new components -> AdminUserList, AdminPollings, NotFound

---
+++ src/components/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+import AvatarGroup from '@material-ui/lab/AvatarGroup';
+
+import useStyles from './styles';
+
+const NotFound: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <h2>Upppsss ....!</h2>
+    </Box>
+  );
+};
+
+export default NotFound;
