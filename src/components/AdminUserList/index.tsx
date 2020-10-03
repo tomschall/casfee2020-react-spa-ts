3ce647b054d56882ddd0281ab357b64d6ad31a5b
webrooster <roland.vonaesch@fhnw.ch>
2020-10-03 19:03:01 +0200

    AdminBoard -> table userlist

--- src/components/AdminUserList/index.tsx
+++ src/components/AdminUserList/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
-import AvatarGroup from '@material-ui/lab/AvatarGroup';
-
+import {
+  Avatar,
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  Typography,
+} from '@material-ui/core';
+import { useGetUserListQuery } from '../../api/generated/graphql';
+import Loader from '../../layout/shared/Loader';
 import useStyles from './styles';
 
 const AdminUserList: React.FC = () => {
   const classes = useStyles();
+  const { data, loading, error } = useGetUserListQuery({
+    variables: {},
+  });
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <>Error loading users</>;
+  }
 
   return (
     <Box className={classes.root}>
@@ -16,6 +38,47 @@ const AdminUserList: React.FC = () => {
         assumenda officia in! Incidunt similique a adipisci facere neque, cum
         nesciunt reprehenderit.
       </p>
+      {data && (
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Avatar</TableCell>
+                <TableCell>Username</TableCell>
+                <TableCell align="right">UserId</TableCell>
+                <TableCell align="left">Auth0</TableCell>
+                <TableCell align="left">Last seen</TableCell>
+              </TableRow>
+            </TableHead>
+            {data.users.map((user) => (
+              <TableBody>
+                <TableRow key={user.id}>
+                  <TableCell>
+                    <Avatar
+                      alt={user.username}
+                      src="https://api.adorable.io/avatars/173/abott@adorable.png"
+                    />
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    <Typography variant="body2">{user.username}</Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography variant="caption">{user.id}</Typography>
+                  </TableCell>
+                  <TableCell align="left">
+                    <Typography variant="body2">
+                      {user.auth0_user_id}
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="left">
+                    {user.last_seen ? user.last_seen : 'Has not logged in yet.'}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            ))}
+          </Table>
+        </TableContainer>
+      )}
     </Box>
   );
 };
