729dceaae34b94a6449f7de6d3e84983672bc58e
webrooster <roland.vonaesch@fhnw.ch>
2020-10-05 12:48:54 +0200

    Polling -> textfield and first entry collapse

--- src/components/AdminPollings/index.tsx
+++ src/components/AdminPollings/index.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
 import {
+  Badge,
   Box,
+  Button,
   Container,
+  Collapse,
+  Divider,
   FormControl,
   FormHelperText,
   Grid,
   Input,
   InputLabel,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  TextField,
+  Typography,
 } from '@material-ui/core';
+import Icon from '@material-ui/core/Icon';
+import HowToVoteIcon from '@material-ui/icons/HowToVote';
+import HelpIcon from '@material-ui/icons/Help';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import EnhancedEncryptionOutlinedIcon from '@material-ui/icons/EnhancedEncryptionOutlined';
+
 import { useGetPollQuestionsQuery } from '../../api/generated/graphql';
 import Loader from '../../layout/shared/Loader';
 import NotFound from '../../layout/shared/NotFound';
@@ -18,6 +35,11 @@ const AdminPollings: React.FC = () => {
   const { data, loading, error } = useGetPollQuestionsQuery({
     variables: {},
   });
+
+  const [open, setOpen] = React.useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
   if (loading) {
     return <Loader />;
@@ -38,17 +60,76 @@ const AdminPollings: React.FC = () => {
         </p>
       </Box>
       <Grid item xs={12}>
-        <FormControl fullWidth>
-          <InputLabel htmlFor="my-input">Question</InputLabel>
-          <Input id="my-input" aria-describedby="my-helper-text" />
-          <FormHelperText id="my-helper-text">
-            Add a meaningful question to the poll.
-          </FormHelperText>
-        </FormControl>
+        <form
+          noValidate
+          autoComplete="off"
+          className={classes.root}
+          // onSubmit={handleSubmit}
+        >
+          <TextField
+            value=""
+            autoFocus={true}
+            // onChange={(e) => {
+            //   handleTyping(e.target.value);
+            // }}
+            size="medium"
+            variant="outlined"
+            color="secondary"
+            autoComplete="off"
+            placeholder="Type your question here ..."
+            id="standard-basic"
+            label="Add a meaningful question"
+            fullWidth
+            InputProps={{
+              classes: {
+                input: classes.messageInput,
+              },
+            }}
+            InputLabelProps={{
+              className: classes.messageInput,
+            }}
+          />
+          <Button
+            size="medium"
+            type={'submit'}
+            variant="contained"
+            endIcon={<Icon>send</Icon>}
+            className={classes.messageButton}
+          >
+            Submit
+          </Button>
+        </form>
+        <Divider className={classes.divider} />
       </Grid>
       <Grid item xs={12}>
         {data?.questions.map((question) => (
-          <p>{question.text}</p>
+          <List className={classes.root}>
+            <ListItem button onClick={handleClick}>
+              <ListItemIcon>
+                <HowToVoteIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography variant="h6">{question.text}</Typography>
+              </ListItemText>
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <List component="div">
+                <ListItem key={question.id} button>
+                  <ListItemIcon>
+                    <HelpIcon color="secondary" />
+                  </ListItemIcon>
+                  <ListItemText primary="Das Huhn" />
+                </ListItem>
+                <ListItem key={question.id} button>
+                  <ListItemIcon>
+                    <HelpIcon color="secondary" />
+                  </ListItemIcon>
+                  <ListItemText primary="Das Ei" />
+                </ListItem>
+              </List>
+            </Collapse>
+          </List>
         ))}
       </Grid>
     </>
--- src/components/AdminPollings/styles.ts
+++ src/components/AdminPollings/styles.ts
@@ -2,4 +2,26 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   root: {},
+  divider: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(5),
+  },
+  messageInput: {
+    floatingLabelFocusStyle: {
+      color: theme.palette.secondary.dark,
+    },
+  },
+  messageButton: {
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+      size: 'small',
+    },
+    [theme.breakpoints.up('md')]: {
+      size: 'large',
+      width: '25%',
+    },
+    marginTop: theme.spacing(2),
+    marginLeft: theme.spacing(0),
+    backgroundColor: theme.palette.primary.dark,
+  },
 }));
