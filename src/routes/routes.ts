5439329c4beeb814dd2e17a815dc095a1718190c
webrooster <roland.vonaesch@fhnw.ch>
2020-10-11 18:12:34 +0200

    WIP -> get public channel query, collapse list -> publish on public channel

---
+++ src/routes/routes.ts
@@ -0,0 +1,18 @@
+export default {
+  base: '/',
+  notfound: '/404-not-found',
+  signed: {
+    base: '/channel',
+    dynamic: '/channel/:channel',
+    general: '/channel/general',
+    dashboard: '/dashboard',
+    users: '/dashboard/users',
+    pollings: '/dashboard/pollings',
+    question: '/dashboard/pollings/edit/question/:question',
+    addUserToChannel: '/add-user-to-channel',
+  },
+  unsigned: {
+    home: '/home',
+    dynamic: '/channel/:channel',
+  },
+};
