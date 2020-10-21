dd5602473e3bda3880125c17eed5c2e57a28aa09
Thomas Schallert <thomas.schallert@fhnw.ch>
2020-10-21 19:23:11 +0200

    add App.test.tsx and test for NotFound component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,38 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the app component', () => {
+  const { container } = render(<App />);
+  expect(container).toMatchInlineSnapshot(`
+  <div>
+    <div
+      class="MuiBox-root MuiBox-root-1"
+      style="height: 100vh; background-color: rgb(26, 35, 126);"
+    >
+      <div
+        class="MuiBox-root MuiBox-root-3"
+      >
+        <div
+          class="MuiCircularProgress-root MuiCircularProgress-colorPrimary MuiCircularProgress-indeterminate"
+          role="progressbar"
+          style="width: 40px; height: 40px;"
+        >
+          <svg
+            class="MuiCircularProgress-svg"
+            viewBox="22 22 44 44"
+          >
+            <circle
+              class="MuiCircularProgress-circle MuiCircularProgress-circleIndeterminate"
+              cx="44"
+              cy="44"
+              fill="none"
+              r="20.2"
+              stroke-width="3.6"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+  `);
 });
---
+++ src/layout/shared/NotFound/index.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import NotFound from './';
+
+test('renders the app component', () => {
+  const { container, getByText } = render(<NotFound />);
+  expect(getByText('Upppsss ....! Something went wrong!')).toBeInTheDocument();
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    <div
+      class="MuiBox-root MuiBox-root-3 makeStyles-root-1"
+    >
+      <h2>
+        Upppsss ....! Something went wrong!
+      </h2>
+    </div>
+  `);
+});
