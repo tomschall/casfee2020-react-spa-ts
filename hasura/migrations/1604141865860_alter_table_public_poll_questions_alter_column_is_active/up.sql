098bdda16be3112d4702d426753fe4e56365e445
webrooster <roland.vonaesch@fhnw.ch>
2020-10-31 13:13:27 +0100

    Migrations -> modify insert conditions -> poll_questions and poll_answers

---
+++ hasura/migrations/1604141865860_alter_table_public_poll_questions_alter_column_is_active/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE ONLY "public"."poll_questions" ALTER COLUMN "is_active" SET DEFAULT false;
---
+++ hasura/migrations/1604141978579_alter_table_public_poll_anwers_alter_column_created_at/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE ONLY "public"."poll_anwers" ALTER COLUMN "created_at" SET DEFAULT now();
