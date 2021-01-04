0bfcb10b7ab0b1745557a272e227b57c4c15e347
webrooster <roland.vonaesch@fhnw.ch>
2021-01-04 11:39:23 +0100

    Threads -> loader

--- src/components/chat/threads/ThreadListContainer.tsx
+++ src/components/chat/threads/ThreadListContainer.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { useWatchThreadsSubscription } from '../../../api/generated/graphql';
 import ThreadList from './ThreadList';
 import { Alert } from '@material-ui/lab';
-import { Box, Grid, makeStyles } from '@material-ui/core';
+import { Box, Grid, makeStyles, Typography } from '@material-ui/core';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Loader from '../../shared/Loader';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,11 +38,36 @@ const useStyles = makeStyles((theme) => ({
 
 const ThreadListContainer: React.FC = () => {
   const classes = useStyles();
-  const { data, error } = useWatchThreadsSubscription();
+  const { data, error, loading } = useWatchThreadsSubscription();
 
   if (error) {
     console.log('error', error);
     return <Alert severity="error">Thread Error</Alert>;
+  }
+
+  if (loading) {
+    return (
+      <>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          style={{ height: '100vh' }}
+        >
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Loader />
+            <Typography variant="caption" color="secondary">
+              Loading threads
+            </Typography>
+          </Box>
+        </Box>
+      </>
+    );
   }
 
   return (
