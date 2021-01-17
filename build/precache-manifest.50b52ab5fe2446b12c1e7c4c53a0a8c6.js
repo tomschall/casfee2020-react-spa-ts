73f83c207ef315ece51af4bdfaf7c8553a923319
Thomas Schallert <thomas.schallert@fhnw.ch>
2021-01-17 21:33:54 +0100

    create bundle

--- build/precache-manifest.50b52ab5fe2446b12c1e7c4c53a0a8c6.js
+++ build/precache-manifest.50b52ab5fe2446b12c1e7c4c53a0a8c6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2939c94d01df09c30c3f71a0cd5fed06",
+    "revision": "27eb8efb3de4609152d1e54e08eace3c",
     "url": "/index.html"
   },
   {
-    "revision": "f61a219300bc8c99fc8a",
+    "revision": "7982637849638cba52d1",
     "url": "/static/css/main.c2ed445e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2afa9ed5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f61a219300bc8c99fc8a",
-    "url": "/static/js/main.0bbb7c68.chunk.js"
+    "revision": "7982637849638cba52d1",
+    "url": "/static/js/main.f05b47f5.chunk.js"
   },
   {
     "revision": "de84326514cbaafcfc39",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f9ff2f775b928153ebd988882e57e98.js"
+  "/precache-manifest.50b52ab5fe2446b12c1e7c4c53a0a8c6.js"
 );
 
 self.addEventListener('message', (event) => {
